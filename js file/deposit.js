874ba564b8a4c22f93977985f710407f8859dc80
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-30 17:55:59 +0600

    deposit ok

---
+++ js file/deposit.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const depositField=document.getElementById('deposit-field');
+    const newDepositAmountString=depositField.value ;
+    const newDepositAmount=parseFloat(newDepositAmountString);
+
+    depositField.value='';
+
+    const depositTotalElement=document.getElementById('deposit-total');
+    const previousDepositTotalString=depositTotalElement.innerText;
+    const previousDepositTotal=parseFloat(previousDepositTotalString);
+
+    const newDepositTotal=previousDepositTotal+newDepositAmount;
+    depositTotalElement.innerText=newDepositTotal
+
+})
